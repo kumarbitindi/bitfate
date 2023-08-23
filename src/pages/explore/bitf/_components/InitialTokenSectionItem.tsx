c9702bfe25907be1c0240a7de995295d2a694542
Kumar <119077822+kumarbitindi@users.noreply.github.com>
2023-08-23 15:18:44 -0600

    Update InitialTokenSectionItem.tsx

--- src/pages/explore/bitf/_components/InitialTokenSectionItem.tsx
+++ src/pages/explore/bitf/_components/InitialTokenSectionItem.tsx
@@ -1,9 +1,10 @@
-import { FiIcon } from "../icons/FiIcon";
-import { AiIcon } from "../icons/AiIcon";
+import { FiSend } from "react-icons/fi";
+import { AiOutlineFire } from "react-icons/ai";
 import classNames from "classnames";
 import { BinIcon } from "../icons/BinIcon";
 
 export function InitialTokenSectionItem({
+  icon,
   percentage,
   desc,
   className,
@@ -13,8 +14,15 @@ export function InitialTokenSectionItem({
   desc: string;
   className?: string;
 }) {
-
-  
+  const Icon = iconMapping[icon];
+  return (
+    <div
+      className={classNames(
+        "flex flex-col md:w-[136.33px] w-[93.5px]",
+        className
+      )}
+    >
+      <Icon className="text-brand-100" size={28} />
       <div className="mt-[18.34px] text-dark-700 text-xl font-bold">
         {percentage}
       </div>
@@ -30,7 +38,7 @@ export enum InitialTokenSectionIcons {
 }
 
 const iconMapping = {
-  [InitialTokenSectionIcons.AIRDROP]: FiIcon,
-  [InitialTokenSectionIcons.BURN]: AiIcon,
+  [InitialTokenSectionIcons.AIRDROP]: FiSend,
+  [InitialTokenSectionIcons.BURN]: AiOutlineFire,
   [InitialTokenSectionIcons.DESTROY]: BinIcon,
 };
