3ca41f4216d5521a5fda145592795618ac38ae75
Kumar <119077822+kumarbitindi@users.noreply.github.com>
2023-08-25 12:23:51 -0600

    Update DFIStatisticsDisplay.tsx

--- src/pages/explore/bitf/_components/DFIStatisticsDisplay.tsx
+++ src/pages/explore/bitf/_components/DFIStatisticsDisplay.tsx
@@ -41,7 +41,7 @@ export function DFIStatisticsDisplay() {
     },
     {
       title: t("statisticsDisplay.dfiMinted.title"),
-      stats: 10290000,
+      stats: 9450000,
       desc:
         supply === undefined
           ? supply
